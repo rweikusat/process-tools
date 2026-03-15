a54ff29f42e04abf14fc6cc9f88e60ebaa9ce474
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-15 20:27:13 +0000

    background in subroutine

--- src/launch.c
+++ src/launch.c
@@ -8,16 +8,9 @@
 #include <syslog.h>
 #include <unistd.h>
 
-/*  main */
-int main(int argc, char **argv)
+/*  routines */
+static void background(void)
 {
-    openlog("launch", LOG_PID | LOG_PERROR, LOG_USER);
-
-    if (argc < 2) {
-        syslog(LOG_WARNING, "Usage: launch <command< <arg>*");
-        exit(1);
-    }
-
     switch (fork()) {
     case -1:
         syslog(LOG_ERR, "%s: fork: %m(%d)", __func__, errno);
@@ -29,6 +22,19 @@ int main(int argc, char **argv)
     default:
         exit(0);
     }
+}
+
+/*  main */
+int main(int argc, char **argv)
+{
+    openlog("launch", LOG_PID | LOG_PERROR, LOG_USER);
+
+    if (argc < 2) {
+        syslog(LOG_WARNING, "Usage: launch <command< <arg>*");
+        exit(1);
+    }
+
+    background();
 
     syslog(LOG_NOTICE, "launching not implemented");
     return 0;
