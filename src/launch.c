dd902f93e048c6ccfd7326a1aed10987fe244432
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-15 20:23:23 +0000

    print a usage message when invoked without arguments

--- src/launch.c
+++ src/launch.c
@@ -3,11 +3,18 @@
 */
 
 /*  includes */
-#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
 
 /*  main */
-int main(void)
+int main(int argc, char **argv)
 {
-    puts("Howdy!");
+    openlog("launch", LOG_PID | LOG_PERROR, LOG_USER);
+
+    if (argc < 2) {
+        syslog(LOG_WARNING, "Usage: launch <command< <arg>*");
+        exit(1);
+    }
+
     return 0;
 }
