5ed59e73ddce6563a795276b2927b495cf09b16a
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-16 16:44:50 +0000

    use and top-level argument handling

--- src/clfds.c
+++ src/clfds.c
@@ -3,9 +3,44 @@
   additional fds asked for via command-line and exec a program
 */
 
-/*  main */
-int main(void)
+/*  includes */
+#include <syslog.h>
+#include <unistd.h>
+
+/*  types */
+struct keep  {
+    struct keep *p;
+    int fd;
+};
+
+/*   routines */
+static void usage(void)
 {
-    puts("Owhi!");
+    syslog(LOG_NOTICE, "Usage: clfds [-k <fd>[,<fd>]* <cmd> <args>*");
+    exit(1);
+}
+
+/*  main */
+int main(int argc, char **argv)
+{
+    struct keep *keep;
+    int c;
+
+    openlog("clfds", LOG_PID | LOG_PERROR, LOG_USER);
+
+    keep = NULL;
+    while (c = getopt(argc, argv, "k:"), c != -1)
+        switch (c) {
+        case 'k':
+            add_keeps(optarg, &keep);
+            break;
+
+        default:
+            usage();
+        }
+
+    argv += optind;
+    if (!argv) usage();
+
     return 0;
 }
