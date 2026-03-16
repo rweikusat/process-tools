b7b78a1bf8b4d4c36ec13c0161756b1596bb1d4b
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-16 16:37:21 +0000

    "Hello, world" clfds

--- src/clfds.c
+++ src/clfds.c
@@ -2,3 +2,10 @@
   close all open file descriptors except 0, 1 and 2 plus any
   additional fds asked for via command-line and exec a program
 */
+
+/*  main */
+int main(void)
+{
+    puts("Owhi!");
+    return 0;
+}
