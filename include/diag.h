e52821d3aee681f891c8434d92cade1da041ef77
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-18 18:18:16 +0000

    diag header complete

--- include/diag.h
+++ include/diag.h
@@ -4,4 +4,24 @@
 #ifndef proc_tools_diag_h
 #define proc_tools_diag_h
 
+/*  includes */
+#include <errno.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+/*  macros */
+define die(sysc) die_(__func__, sysc)
+
+/*  routines */
+static void init_diag(char *name)
+{
+    openlog(name, LOG_PID | LOG_PERROR, LOG_USER);
+}
+
+static die_(char const *fnc, char const *sysc)
+{
+    syslog(LOG_ERR, "%s: %s: %m(%d)", fnc, sysc, errno);
+    exit(1);
+}
+
 #endif
