0e9da40ec454be5541f42c4e75003f470b30310b
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-19 14:37:13 +0000

    argument parser but no implmentation

--- src/sane_env.c
+++ src/sane_env.c
@@ -4,12 +4,36 @@
 
 /*  includes */
 #include "diag.h"
+#include <unistd.h>
+
+/*  routines */
+static void usage(void)
+{
+    syslog(LOG_NOTICE, "Usage: sane-env [-k <keep>] [-s <set>] <cmd> <arg>*");
+    syslog(LOG_NOTICE, "    Both options can appear any number of times. Keep arguments name vars to keep.");
+    syslog(LOG_NOTICE, "    Set arguments must be of the form <name>=<value>. Will be added.");
+    exit(1);
+}
 
 /*  main */
-int main(void)
+int main(int argc, char **argv)
 {
+    int c;
+
     init_diag("sane-env");
-    syslog(LOG_NOTICE, "Ha!");
+    while (c = getopt(argc, argv, "k:s:"), c != -1)
+        switch (c) {
+        case 'k':
+            syslog(LOG_NOTICE, "keep %s", optarg);
+            break;
+
+        case 's':
+            syslog(LOG_NOTICE, "set %s", optarg);
+            break;
+
+        default:
+            usage();
+        }
 
     return 0;
 }
