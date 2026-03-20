00560e841801619a6fa3b4bb3e4c504fbdaf5f91
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-20 11:43:46 +0000

    memory allocation

---
+++ include/alloc.h
@@ -0,0 +1,22 @@
+/*
+  simple memory allocation
+*/
+#ifndef proc_tools_alloc_h
+#define proc_tools_alloc_h
+
+/*  includes */
+#include <sys/types.h>
+#include <unistd.h>
+
+/*  macros */
+#define PTR_SZ	sizeof(void *)
+
+/*  routines */
+static void *alloc(size_t sz)
+{
+    /*  ensure that allocation size is a multiple of ptr size */
+    sz = (sz + PTR_SZ - 1) & ~(PTR_SZ - 1);
+    return sbrk(sz);
+}
+
+#endif
