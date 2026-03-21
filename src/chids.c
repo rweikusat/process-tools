84637a3a6a35a83611fb0d7b96291e58d2a6f5ff
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-21 12:36:24 +0000

    argument handling

--- src/chids.c
+++ src/chids.c
@@ -3,12 +3,47 @@
 */
 
 /*  includes */
+#include <unistd.h>
+
 #include "diag.h"
 
-/*  main */
-int main(void)
+/*  routines */
+static void usage(void)
 {
+    syslog(LOG_NOTICE, "Usage: chids [-g <group>] [-u <user>]");
+    exit(1);
+}
+
+/*  main */
+int main(int argc, char **argv)
+{
+    char *group, *user;
+    int c;
+
     init_diag("chids");
-    syslog(LOG_NOTICE, "Ha!");
+
+    user = group = NULL;
+    while (c = getopt(argc, argv, "u:g:"), c != -1)
+        switch (c) {
+        case 'g':
+            group = optarg;
+            break;
+
+        case 'u':
+            user = optarg;
+            break;
+
+        default:
+            usage();
+        }
+
+    if (!(user || group)) {
+        syslog(LOG_NOTICE, "either -u or -g must be provided");
+        exit(1);
+    }
+
+    argv += optind;
+    if (!*argv) usage();
+
     return 0;
 }
