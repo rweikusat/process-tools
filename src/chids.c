fb5177ec819d1820682f22987d17f9c05fd07d08
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-21 12:55:30 +0000

    should be complete

--- src/chids.c
+++ src/chids.c
@@ -3,6 +3,8 @@
 */
 
 /*  includes */
+#include <grp.h>
+#include <pwd.h>
 #include <unistd.h>
 
 #include "diag.h"
@@ -12,6 +14,34 @@ static void usage(void)
 {
     syslog(LOG_NOTICE, "Usage: chids [-g <group>] [-u <user>] <cmd> <arg>*");
     exit(1);
+}
+
+static void change_gid(char *group)
+{
+    struct group *grp;
+    gid_t gid;
+    int rc;
+
+    grp = getgrnam(group);
+    if (grp) gid = grp->gr_gid;
+    else grp = atoi(group);
+
+    rc = setgid(gid);
+    if (rc == -1) die("setgid");
+}
+
+static void change_uid(char *user)
+{
+    struct passwd *pwd;
+    uid_t uid;
+    int rc;
+
+    pwd = getpwnam(user);
+    if (pwd) uid = pwd->pw_uid;
+    else uid = atoi(user);
+
+    rc = setuid(uid);
+    if (rc == -1) die("setuid")
 }
 
 /*  main */
