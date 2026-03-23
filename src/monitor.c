5cc42c2fa28904bf9d69e6f3ba7c0ed14a0d8daa
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-23 17:57:27 +0000

    top-level init

--- src/monitor.c
+++ src/monitor.c
@@ -3,7 +3,19 @@
 */
 
 /*  includes */
+#include <signal.h>
+
 #include "diag.h"
+
+/*  constants */
+enum {
+    DEF_GRACE = 20`             /* seconds */
+};
+
+/*  variables */
+static char *name, **cmdv;
+static unsigned grace = DEF_GRACE;
+static int term_sig = SIGTERM;
 
 /*  routines */
 static void usage(void)
@@ -15,11 +27,48 @@ static void usage(void)
     exit(1);
 }
 
+static void init(int argc, char **argv)
+{
+    char *ctrl_grp;
+    int c;
+
+    ctrl_grp = NULL;
+    while (c = getopt(argc, argv, "g:n:p:t:"), c != -1)
+        switch (c) {
+        case 'g':
+            ctrl_grp = optarg;
+            break;
+
+        case 'n':
+            name = optarg;
+            break;
+
+        case 'p':
+            grace = atoi(optarg);
+            break;
+
+        case 't':
+            term_sig = atoi(optarg);
+            break;
+
+        default:
+            usage();
+        }
+
+    argv += optind;
+    if (!*argv) usage();
+    cmdv = argv;
+
+    if (!name) name = *cmdv;
+    create_ctrl(name, ctrl_grp);
+}
+
 /*  main */
 int main(int argc, char **argv)
 {
     init_diag("monitor");
-    if (argc < 2) usage();
+
+    init(argc, argv);
 
     return 0;
 }
