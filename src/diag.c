afc24cc28362886143c422eb29f72d05fd6efd4e
Rainer Weikusat <rweikusat@talktalk.net>
2026-03-23 13:57:21 +0000

    err

--- src/diag.c
+++ src/diag.c
@@ -13,8 +13,22 @@ void init_diag(char *name)
 
 void die_(char const *fnc, char const *sysc)
 {
-    syslog(LOG_ERR, "%s: %s: %m(%d)", fnc, sysc, errno);
+    err("%s: %s: %m(%d)", fnc, sysc, errno);
     exit(1);
+}
+
+static void log_msg(int prio, char *tmpl, va_list val)
+{
+    vsyslog(prio, tmpl, val);
+    va_end(val);
+}
+
+void err(char *tmpl, ...)
+{
+    va_list val;
+
+    va_start(val, tmpl);
+    log_msg(LOG_ERR, tmpl, val);
 }
 
 void msg(char *tmpl, ...)
@@ -22,6 +36,5 @@ void msg(char *tmpl, ...)
     va_list val;
 
     va_start(val, tmpl);
-    vsyslog(LOG_NOTICE, tmpl, val);
-    va_end(val);
+    log_msg(LOG_NOTICE, tmpl, val);
 }
