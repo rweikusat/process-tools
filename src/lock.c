4b956957b292bf8bccbcaaa98577edf786cb063a
Rainer Weikusat <rweikusat@talktalk.net>
2026-04-08 15:33:21 +0100

    main & usage

--- src/lock.c
+++ src/lock.c
@@ -3,12 +3,44 @@
 */
 
 /*  includes */
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "diag.h"
 
-/*  main */
-int main(void)
+/*  routines */
+static void usage(void)
 {
+    msg("Usage: lock <-r|-w file>* <cmd> <args>*");
+    msg("    Execute a command with a certain set of POSIX record locks held.");
+    msg("    The -r option denotes a read lock on a file that's to be acquired,");
+    msg("    -w a write lock. Both can appear any number of times.");
+
+    exit(1);
+}
+
+/*  main */
+int main(int argc, char **argv)
+{
+    int c;
+
     init_diag("lock");
-    msg("Ho ho ho!");
+    while (c = getopt(argc, argv, "r:w:"), c != -1)
+        switch (c) {
+        case 'r':
+        case 'w':
+            lock(optarg, c);
+            break;
+
+        default:
+            usage();
+        }
+
+    argv += optind;
+    if (!*argv) usage();
+
+    execvp(*argv, argv);
+
+    sys_die("execvp");
     return 0;
 }
