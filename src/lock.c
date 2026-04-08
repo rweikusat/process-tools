4202e616a15a076e639b212ebc7fdf1e83dbd33c
Rainer Weikusat <rweikusat@talktalk.net>
2026-04-08 16:25:19 +0100

    added some diagnostics and a quiet flag

--- src/lock.c
+++ src/lock.c
@@ -10,16 +10,25 @@
 
 #include "diag.h"
 
+/*  variables */
+static int quiet;
+
+static char *tps[] = {
+    [F_RDLCK] =	"READ",
+    [F_WRLCK] = "WRITE"
+};
+
 /*  routines */
 static void usage(void)
 {
-    msg("Usage: lock ([-t] -r|w <file>)* <cmd> <args>*");
+    msg("Usage: lock [-q] ([-t] -r|w <file>)* <cmd> <args>*");
     msg("    Execute a command with a certain set of POSIX record locks held.");
     msg("    The -r option denotes a read lock on a file that's to be acquired,");
     msg("    -w a write lock. Both can appear any number of times. Using -t");
     msg("    requests that the next attempted lock operation will be a trylock,");
     msg("    that is, the program will exit with an error status code if the ");
     msg("    lock cannot be acquired without waiting.");
+    msg("    The -q option can be used to disable information messages.");
 
     exit(1);
 }
@@ -29,31 +38,35 @@ static void lock(char *path, int kind, int op)
     struct flock lk;
     int fd, o_flags, rc;
 
+    o_flags = 0;                /* silence pointless warning */
     switch (kind) {
     case 'r':
         o_flags = O_RDONLY;
-        lk.l_type = F_RDLK;
+        lk.l_type = F_RDLCK;
         break;
 
     case 'w':
         o_flags = O_RDWR;
-        lk.l_type = F_WRLK;
+        lk.l_type = F_WRLCK;
     }
 
+    if (!quiet)
+        msg("trying to acquire %s lock on %s",
+            tps[lk.l_type], path);
+
     fd = open(path, o_flags, 0);
-    if (fd == -1) {
-        msg("file %s", path);
-        sys_die("open");
-    }
+    if (fd == -1) die("open");
 
     lk.l_whence = lk.l_start = lk.l_len = 0;
     rc = fcntl(fd, op, &lk);
     if (rc == -1) {
         if (op == F_SETLK &&
-            (errno == EACCES || errno == EAGAIN))
+            (errno == EACCES || errno == EAGAIN)) {
+            msg("failed");
             exit(1);
+        }
 
-        sys_die("fcntl");
+        die("fcntl");
     }
 }
 
@@ -65,9 +78,12 @@ int main(int argc, char **argv)
     init_diag("lock");
     op = F_SETLKW;
 
-    while (c = getopt(argc, argv, "r:w:t"), c != -1) {
-
+    while (c = getopt(argc, argv, "qr:w:t"), c != -1) {
         switch (c) {
+        case 'q':
+            quiet = 1;
+            break;
+
         case 'r':
         case 'w':
             lock(optarg, c, op);
@@ -88,6 +104,6 @@ int main(int argc, char **argv)
 
     execvp(*argv, argv);
 
-    sys_die("execvp");
+    die("execvp");
     return 0;
 }
