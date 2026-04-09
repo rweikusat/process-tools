a26d549c7595f2378dab3e6d38a03b4c7e4fe603
Rainer Weikusat <rweikusat@talktalk.net>
2026-04-09 14:54:31 +0100

    usage & option parsing

--- src/have_locks.c
+++ src/have_locks.c
@@ -4,12 +4,47 @@
 */
 
 /*  includes */
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "diag.h"
 
-/*  main */
-int main(void)
+/*  variables */
+static int verbose;
+
+/*  routines */
+static void usage(void)
 {
-    init_diag("have_locks");
-    msg("Yo!");
+    msg("Usage: have-locks [-v] </path/to/file>+");
+    msg("    Determines if locks on all files are held somwhere within");
+    msg("    the current process scope, that is, the set of all processes");
+    msg("    part of the ppid chain starting with the ppid of the current");
+    msg("    process and ending with pid 1.");
+    msg("    The -v option can be used to request printing of informational");
+    msg("    messages.");
+
+    exit(1);
+}
+
+/*  main */
+int main(int argc, char **argv)
+{
+    int c;
+
+    init_diag("have-locks");
+
+    while (c = getopt(argc, argv, "+v"), c != -1)
+        switch (c) {
+        case 'v':
+            verbose = 1;
+            break;
+
+        default:
+            usage();
+        }
+
+    argv += optind;
+    if (!*argv) usafge();
+
     return 0;
 }
