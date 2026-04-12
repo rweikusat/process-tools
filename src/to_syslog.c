661a6ea435dd5322567538a7a73ba98bf726712c
Rainer Weikusat <rweikusat@talktalk.net>
2026-04-12 19:34:38 +0100

    usage

--- src/to_syslog.c
+++ src/to_syslog.c
@@ -4,7 +4,24 @@
 */
 
 /*  includes */
+#include <stdlib.h>
+
 #include "diag.h"
+
+/*  routines */
+static void usage(void)
+{
+    msg("Usage: to-syslog [-f <fd>[,<fd>*] <cmd> <arg>*");
+    msg("    Execute a command and relay output on certain file descriptors");
+    msg("    (default: 1 and 2) to syslog.");
+    msg("    The -f option can be used to specify a different set of file");
+    msg("    descriptors.);
+    msg("    Output lines matching the Perl pattern '^\w\[0-9+\]: ' won't be");
+    msg("    won't be relayed as it's assumed that they were already sent to");
+    msg("    syslog.");
+
+    exit(1);
+}
 
 /*  main */
 int main(void)
