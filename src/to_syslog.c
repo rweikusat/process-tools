2f4f1c23afa3784b467181cadce5735dbb1f82b0
Rainer Weikusat <rweikusat@talktalk.net>
2026-04-12 19:39:49 +0100

    top-level argument parsing

--- src/to_syslog.c
+++ src/to_syslog.c
@@ -8,6 +8,21 @@
 
 #include "diag.h"
 
+/*  types */
+struct relay_fd {
+    struct relay_fd *p;
+    int from, to;
+};
+
+/*  variables */
+static struct relay_fd def_relay[] = {
+    {
+        .p = def_relay + 1,
+        .to = 1 },
+    {
+        .to = 2 }
+};
+
 /*  routines */
 static void usage(void)
 {
@@ -15,8 +30,8 @@ static void usage(void)
     msg("    Execute a command and relay output on certain file descriptors");
     msg("    (default: 1 and 2) to syslog.");
     msg("    The -f option can be used to specify a different set of file");
-    msg("    descriptors.);
-    msg("    Output lines matching the Perl pattern '^\w\[0-9+\]: ' won't be");
+    msg("    descriptors.");
+    msg("    Output lines matching the Perl pattern '^\\w\\[0-9+\\]: ' won't be");
     msg("    won't be relayed as it's assumed that they were already sent to");
     msg("    syslog.");
 
@@ -24,9 +39,25 @@ static void usage(void)
 }
 
 /*  main */
-int main(void)
+int main(int argc, char **argv)
 {
+    struct relay_fd *relays;
+    int c;
+
     init_diag("to-syslog");
-    msg("Hab Gubmuh!");
+    relays = def_realy;
+    while (c = getopt(argc, argv, "+f"), c != -1)
+        switch (c) {
+        case 'f':
+            relays = parse_fd_lst(optarg);
+            break;
+
+        default:
+            usage();
+        }
+
+    argv += optind;
+    if (!*argv) usage();
+
     return 0;
 }
