2269b31d0e4fad7b663da6f88c29336f51c5b3e7
Rainer Weikusat <rweikusat@talktalk.net>
2026-04-24 20:00:23 +0100

    some buffer stuff/ top-level socket creation support

--- src/u_talk.c
+++ src/u_talk.c
@@ -8,6 +8,19 @@
 
 #include "diag.h"
 
+/*  constants */
+enum {
+    BUFSZ =	4096
+};
+
+/*  types */
+struct my_sk {
+    int sk, passive;
+};
+
+/*  routines */
+static unsigned buf_sz = DEF_BUFSZ;
+
 /*  routines */
 static void usage(void)
 {
@@ -26,14 +39,41 @@ static void usage(void)
     exit(1);
 }
 
-static void init(int argc, char **argv)
+static void create_socket(char *addr, struct my_sk *my_sk)
+{
+    struct sockaddr_un sun;
+    unsigned sun_len;
+    int rc;
+
+    rc = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (rc == -1) die("socket");
+    my_sk->sk = rc;
+
+    fill_sun(addr, &sun, &sun_len);
+
+    if (my_sk->passive) {
+        listen_on(my_sk->sk, &sun);
+        return;
+    }
+
+    unlink(sun.sun_path);
+    rc = connect(my_sk->sk, (struct sockaddr *)&sun, sun_len);
+    if (rc == -1) die("connect");
+}
+
+static void init(int argc, char **argv, struct my_sk *my_sk)
 {
     int c;
 
+    my_sk->passive = 0;
     while (c = getopt(argc, argv, "+b:p"), c != -1)
         switch (c) {
         case 'b':
+            buf_sz = atoi(optarg);
+            break;
+
         case 'p':
+            my_sk->passive = 1;
             break;
 
         default:
@@ -42,12 +82,16 @@ static void init(int argc, char **argv)
 
     argv += optind;
     if (!*argv) usage();
+
+    create_socket(*argv, my_sk);
 }
 
 /*  main */
 int main(int argc, char **argv)
 {
+    struct my_sk my_sk;
+
     init_diag("u-talk");
-    init(argc, argv);
+    init(argc, argv, &my_sk);
     return 0;
 }
