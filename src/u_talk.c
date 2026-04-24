e8db884335cdf702ca0fcd24aa18eb7f6647b8fa
Rainer Weikusat <rweikusat@talktalk.net>
2026-04-24 20:38:05 +0100

    fill_sun

--- src/u_talk.c
+++ src/u_talk.c
@@ -39,6 +39,34 @@ static void usage(void)
     exit(1);
 }
 
+static void fill_sun(char *addr,
+                     struct sockaddr_un *sun, unsigned *sun_len)
+{
+    size_t a_len;
+    char *a_dst;
+
+    sun->sun_family = AF_UNIX;
+    s_len =
+    a_len = strlen(addr);
+    a_dst = sun->sun_path;
+
+    if (*addr == '/' && addr[1] == '/') {
+        a_len -= 2;
+        addr += 2;
+
+        *a_dst++ = 0;
+    } else
+        ++a_len;
+
+    if (a_len > (sun->sun_path + sizeof(sun->sun_path)) - a_dst) {
+        err("%s: addr too large", __func__);
+        exit(1);
+    }
+
+    memcpy(a_dst, addr, a_len);
+    *sun_len = offsetof(struct sockaddr_un, sun_path) + a_len;
+}
+
 static void create_socket(char *addr, struct my_sk *my_sk)
 {
     struct sockaddr_un sun;
