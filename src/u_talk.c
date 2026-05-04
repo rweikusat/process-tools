e9d93e9a160c84b038fb035cbb4d044b28c20fc8
Rainer Weikusat <rweikusat@talktalk.net>
2026-05-04 12:26:19 +0100

    v2 u-talk -- support executing arbitrary commands/ don't support passive open anymore

--- src/u_talk.c
+++ src/u_talk.c
@@ -3,119 +3,56 @@
 */
 
 /*  includes */
-#include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
 #include <sys/un.h>
 #include <unistd.h>
 
 #include "diag.h"
-
-/*  macros */
-#define DEF_BUFSZ	"4096"
-
-/*  types */
-struct my_sk {
-    int sk, passive;
-};
-
-/*  routines */
-static char *buf_sz = DEF_BUFSZ;
+#include "fill_sun.h"
 
 /*  routines */
 static void usage(void)
 {
-    msg("Usage: u-talk [-b <bufsize>] [-p] <socket>");
-    msg("    Communicate using AF_UNIX stream sockets.");
-    msg("    Connect to the socket specified by <socket> and relay data");
-    msg("    back and forth. If <socket> starts with '//', it refers to a ");
-    msg("    socket in the Linux abstract namespace for AF_UNIX sockets.");
-    msg("    The -p option can be used to demand a passive open, that is");
-    msg("    creation of a server socket instead. This is intended for ");
-    msg("    testing and only one client connect is supported. The socket");
-    msg("    will be created with 0600 access permissions.");
-    msg("    The -b option can be used to use a different I/O buffer size");
-    msg("    than the default of 4096.");
+    msg("Usage: u-talk [-p] <socket> [<cmd> <arg>*]");
+    msg("    Connect to the AF_UNIX socket speciffied by <socket>. If");
+    msg("    <socket> starts with '//', connect to a socket in the Linux ");
+    msg("    abstract namespace whose name is the remainder of the string.");
+    msg("    When the optional <cmd> argument is passed, the specified");
+    msg("    command will be executed with stdin and stdout referring to the");
+    msg("    connected socket. Otherwise, data will be relayed between stdin");
+    msg("    and stdout of the process and the connected socket.");
+    msg("    The -p option can be used to request using a SOCK_SEQPACKET");
+    msg("    instead of a SOCK_STREAM socket.");
 
     exit(1);
 }
 
-static void fill_sun(char *addr,
-                     struct sockaddr_un *sun, unsigned *sun_len)
-{
-    size_t a_len;
-    char *a_dst;
-
-    sun->sun_family = AF_UNIX;
-    a_len = strlen(addr);
-    a_dst = sun->sun_path;
-
-    if (*addr == '/' && addr[1] == '/') {
-        a_len -= 2;
-        addr += 2;
-
-        *a_dst++ = 0;
-    } else
-        ++a_len;
-
-    if (a_len > (sun->sun_path + sizeof(sun->sun_path)) - a_dst) {
-        err("%s: addr too large", __func__);
-        exit(1);
-    }
-
-    memcpy(a_dst, addr, a_len);
-    *sun_len = offsetof(struct sockaddr_un, sun_path) + a_len;
-}
-
-static void create_socket(char *addr, struct my_sk *my_sk)
+static int connect_to(char *addr, int type)
 {
     struct sockaddr_un sun;
     unsigned sun_len;
-    int rc, omask;
+    int rc, sk;
 
-    rc = socket(AF_UNIX, SOCK_STREAM, 0);
-    if (rc == -1) die("socket");
-    my_sk->sk = rc;
+    sk = socket(AF_UNIX, type, 0);
+    if (sk == -1) die("socket");
 
     fill_sun(addr, &sun, &sun_len);
-
-    if (my_sk->passive) {
-        if (*sun.sun_path) {
-            unlink(sun.sun_path);
-            omask = umask(~0600);
-        }
-
-        rc = bind(my_sk->sk, (struct sockaddr *)&sun, sun_len);
-        if (rc == -1) die("bind");
-
-        if (*sun.sun_path) umask(omask);
-
-        rc = listen(my_sk->sk, 10);
-        if (rc == -1) die("listen");
-
-        return;
-    }
-
-    rc = connect(my_sk->sk, (struct sockaddr *)&sun, sun_len);
+    rc = connect(sk, (struct sockaddr *)&sun, sun_len);
     if (rc == -1) die("connect");
+
+    return sk;
 }
 
-static void init(int argc, char **argv, struct my_sk *my_sk)
+static int init(int argc, char **argv)
 {
-    int c;
+    int c, type;
 
-    my_sk->passive = 0;
-    while (c = getopt(argc, argv, "+b:p"), c != -1)
+    type = SOCK_STREAM;
+    while (c = getopt(argc, argv, "p"), c != -1)
         switch (c) {
-        case 'b':
-            buf_sz = optarg;
-            break;
-
         case 'p':
-            my_sk->passive = 1;
+            type = SOCK_SEQPACKET;
             break;
 
         default:
@@ -125,7 +62,21 @@ static void init(int argc, char **argv, struct my_sk *my_sk)
     argv += optind;
     if (!*argv) usage();
 
-    create_socket(*argv, my_sk);
+    return connect_to(*argv, type);
+}
+
+static void exec_cmd(int sk, char **argv)
+{
+    int rc;
+
+    rc = dup2(sk, 0);
+    if (rc == -1) die("dup2/0");
+    rc = dup2(sk, 1);
+    if (rc == -1) die("dup2/1");
+    close(sk);
+
+    execvp(*argv, argv);
+    die("execvp");
 }
 
 static void exec_relay(int sk)
@@ -140,29 +91,15 @@ static void exec_relay(int sk)
 /*  main */
 int main(int argc, char **argv)
 {
-    struct my_sk my_sk;
-    int acc_sk;
+    int sk;
 
     init_diag("u-talk");
-    init(argc, argv, &my_sk);
+    sk = init(argc, argv);
 
-    if (my_sk.passive)
-        while (1) {
-            acc_sk = accept(my_sk.sk, NULL, NULL);
-            if (acc_sk == -1) die("accept");
+    argv += optind;
+    ++argv;
+    if (*argv) exec_cmd(sk, argv);
+    exec_relay(sk);
 
-            switch (fork()) {
-            case -1:
-                die("fork");
-
-            case 0:
-                exec_relay(acc_sk);
-            }
-
-            close(acc_sk);
-            wait(NULL);
-        }
-
-    exec_relay(my_sk.sk);
     return 0;
 }
