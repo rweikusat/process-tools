9900226f925f798fbede8a44a458aeca6149dd59
Rainer Weikusat <rweikusat@talktalk.net>
2026-05-04 12:20:19 +0100

    fill_sun header

---
+++ include/fill_sun.h
@@ -0,0 +1,14 @@
+/*
+  initializae a sockaddr_un structure
+*/
+#ifndef proc_tools_fill_sun_h
+#define proc_tools_fill_sun_h
+
+/*  types */
+struct sockaddr_un;
+
+/*  routines */
+void fill_sun(char *addr,
+              struct sockaddr_un *sun, unsigned *sun_len);
+
+#endif
