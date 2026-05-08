4f1fda4e69b7068626fc809dd45851a7c1ccbb9e
Rainer Weikusat <rweikusat@talktalk.net>
2026-05-08 14:52:48 +0100

    main adapted for tcp listen

--- src/t_listen.c
+++ src/t_listen.c
@@ -1,5 +1,5 @@
 /*
-  u-listen --- AF_UNIX communication/ passive
+  t-listen --- TCP communication/ passive
 */
 
 /*  includes */
@@ -16,88 +16,19 @@
 /*  routines */
 static void usage(void)
 {
-    msg("Usage: u-listen [-g <group>] [-p] <socket> [<cmd> <arg>*]");
-    msg("    Create a listening AF_UNIX socket bound to the address <socket>. ");
-    msg("    If <socket> starts with '//', an address in the Linux abstract");
-    msg("    namespace whose name is the remainder of the string will be used.");
-    msg("    The accept program will be invoked to handle actual client");
-    msg("    connections.");
-    msg("    The -g option can be used to specify a group for the listening socket.");
-    msg("    If provided, it will be made writeable by this group.");
-    msg("    The -p option can be used to request using a SOCK_SEQPACKET");
-    msg("    instead of a SOCK_STREAM socket.");
+    msg("Usage: t-listen <port>[@<addr>] [<cmd> <arg>*]");
+    msg("    Create a TCP socket listening on <port>. If the optional");
+    msg("    @<addr> is present, the socket will be bound to the address");
+    msg("    specified by <addr>. Otherwise, the wildcard address will be");
+    msg("    used. The accept program will be invoked to handle actual");
+    msg("    client connections and any arguments after the first will");
+    msg("    be passed to it.");
 
     exit(1);
 }
 
-static gid_t gid_for(char *group)
+static int listen_on(char *addr)
 {
-    struct group *grp;
-
-    grp = getgrnam(group);
-    return grp ? grp->gr_gid : atoi(group);
-}
-
-static int listen_on(char *addr, int type, char *group)
-{
-    struct sockaddr_un sun;
-    unsigned sun_len;
-    mode_t mode;
-    gid_t gid;
-    int rc, sk;
-
-    sk = socket(AF_UNIX, type, 0);
-    if (sk == -1) die("socket");
-
-    fill_sun(addr, &sun, &sun_len);
-    if (*sun.sun_path) unlink(sun.sun_path);
-    rc = bind(sk, (struct sockaddr *)&sun, sun_len);
-    if (rc == -1) die("bind");
-
-    if (*sun.sun_path) {
-        if (group) {
-            gid = gid_for(group);
-            rc = chown(sun.sun_path, -1, gid);
-            if (rc == -1) die("chown");
-
-            mode = 0660;
-        } else
-            mode = 0600;
-        rc = chmod(sun.sun_path, mode);
-        if (rc == -1) die("chmod");
-    }
-
-    rc = listen(sk, 10);
-    if (rc == -1) die("listen");
-
-    return sk;
-}
-
-static int create_socket(int argc, char **argv)
-{
-    char *group;
-    int c, type;
-
-    group = NULL;
-    type = SOCK_STREAM;
-    while (c = getopt(argc, argv, "+g:p"), c != -1)
-        switch (c) {
-        case 'g':
-            group = optarg;
-            break;
-
-        case 'p':
-            type = SOCK_SEQPACKET;
-            break;
-
-        default:
-            usage();
-        }
-
-    argv += optind;
-    if (!*argv) usage();
-
-    return listen_on(*argv, type, group);
 }
 
 /*  main */
@@ -108,11 +39,13 @@ int main(int argc, char **argv)
 
     init_diag("u-listen");
 
-    sk = create_socket(argc, argv);
-    ++optind;
-    argv += optind;
+    if (argc < 2) usage();
 
-    p = accv = alloca((3 + argc - optind) * sizeof(*accv));
+    ++argv;
+    sk = listen_on(*argv);
+
+    ++argv;
+    p = accv = alloca((3 + argc - 2) * sizeof(*accv));
     *p++ = "accept";
     sprintf(sks, "%d", sk);
     *p++ = sks;
