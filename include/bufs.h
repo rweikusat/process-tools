94f7729237ac7eeee5d245dec602fed9267f0770
Rainer Weikusat <rweikusat@talktalk.net>
2026-05-10 13:08:51 +0100

    bufs header

---
+++ include/bufs.h
@@ -0,0 +1,23 @@
+/*
+  relay buffer management
+*/
+#ifndef proc_tools_bufs_h
+#define proc_tools_bufs_h
+
+/*  types */
+struct buf {
+    struct buf *p;
+    char *s, *e;
+};
+
+/*  routines */
+static inline void reset_buf(struct buf *buf)
+{
+    buf->s = buf->e = (char *)(buf + 1);
+}
+
+struct buf *get_buf(void);
+void return_buf(struct buf *buf);
+void init_buffers(size_t buf_sz, size_t max_bufs);
+
+#endif
