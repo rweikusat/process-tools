2dce7655fd6e91b660c168a535e0d5827f3e07ef
Rainer Weikusat <rweikusat@talktalk.net>
2026-05-12 12:37:27 +0100

    set pipe fds to non-blocking

--- src/pipe.c
+++ src/pipe.c
@@ -4,6 +4,7 @@
 
 /*  includes */
 #include <errno.h>
+#include <fcntl.h>
 #include <sys/poll.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -107,11 +108,18 @@ static int handle_output(int fd, struct io *io)
     return 0;
 }
 
+static inline void set_nonblocking(int fd)
+{
+    fcntl(fd, F_SETFL, fcntl(input->io.fd, F_GETFL) | O_NONBLOCK);
+}
+
 void init_pipe(int r_fd, int w_fd, struct pipe *feeder, struct pipe *pipe)
 {
+    set_nonblocking(r_fd);
     pipe->rd.fd = r_fd;
     pipe->rd.handler = handle_input;
 
+    set_nonblocking(w_fd);
     pipe->wr.fd = w_fd;
     pipe->wr.handler = handle_output;
 
