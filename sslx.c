650e72c23caa7ba599c1b270899c5cf9b1922ab5
Rainer Weikusat <rweikusat@talktalk.net>
2026-05-15 17:52:18 +0100

    do a complete handshake in memory

--- sslx.c
+++ sslx.c
@@ -60,7 +60,7 @@ int main(void)
     SSL *ssl_c, *ssl_s;
     BIO *rbio_c, *wbio_c, *rbio_s, *wbio_s;
     struct data data;
-    int rc;
+    int rc_c, rc_s;
 
     ctx_c = SSL_CTX_new(TLS_client_method());
     ssl_c = SSL_new(ctx_c);
@@ -73,23 +73,29 @@ int main(void)
     rbio_s = BIO_new(BIO_s_mem());
     wbio_s = BIO_new(BIO_s_mem());
     SSL_set_bio(ssl_s, rbio_s, wbio_s);
-    rc = SSL_use_certificate_file(ssl_s, "cert", SSL_FILETYPE_PEM);
-    if (rc == 1) rc = SSL_use_PrivateKey_file(ssl_s, "cert", SSL_FILETYPE_PEM);
-    if (rc != 1) {
+    rc_s = SSL_use_certificate_file(ssl_s, "cert", SSL_FILETYPE_PEM);
+    if (rc_s == 1) rc_s = SSL_use_PrivateKey_file(ssl_s, "cert", SSL_FILETYPE_PEM);
+    if (rc_s != 1) {
         fputs("failed to load cert or key\n", stderr);
         exit(1);
     }
 
-    while (rc = SSL_connect(ssl_c), rc == -1) {
-        assert_want_read(ssl_c, rc);
+    while (rc_c = SSL_connect(ssl_c), rc_c == -1) {
+        assert_want_read(ssl_c, rc_c);
         get_data(wbio_c, &data);
 
         put_data(&data, rbio_s);
-        rc = SSL_accept(ssl_s);
-        if (rc != 1) assert_want_read(ssl_s, rc);
+        rc_s = SSL_accept(ssl_s);
+        if (rc_s != 1) assert_want_read(ssl_s, rc_s);
         get_data(wbio_s, &data);
 
         put_data(&data, rbio_c);
+    }
+
+    if (rc_s == -1) {
+        get_data(wbio_c, &data);
+        put_data(&data, rbio_s);
+        rc_s = SSL_accept(ssl_s);
     }
 
     return 0;
