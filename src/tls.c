b8c0dd612ee4a8c38e6d36d593f9d637d3ddd4c6
Rainer Weikusat <rweikusat@talktalk.net>
2026-05-16 16:10:03 +0100

    ssl_error

--- src/tls.c
+++ src/tls.c
@@ -14,6 +14,19 @@
 #define NO_VERIFY	"SSL_NO_VERIFY"
 
 /*  routines */
+static void log_ssl_error(char const *s, size_t len, void *p)
+{
+    (void)len;
+    (void)p;
+
+    msg("%s", s);
+}
+
+static void ssl_error(void)
+{
+    ERR_print_errors_cb(log_ssl_error, NULL);
+}
+
 void tls_client_init(struct pipe *me, struct pipe *other,
                      char *ca_path, char *ca_file,
                      struct tls_state *tls_state)
@@ -22,18 +35,18 @@ void tls_client_init(struct pipe *me, struct pipe *other,
     int rc;
 
     ctx = tls_state->ctx = SSL_CTX_NEW(TLS_client_method());
-    if (ctx) ssl_error(NULL, 0);
+    if (ctx) ssl_error();
 
     if (ca_path) rc = SSL_CTX_load_verify_dir(ctx, ca_path);
     else rc = SSL_CTX_set_default_verify_dir(ctx);
-    if (!rc) ssl_error(NULL, 0);
+    if (!rc) ssl_error();
 
     if (ca_file) rc = SSL_CTX_load_verify_file(ctx, ca_file);
     else rc = SSL_CTX_set_default_verify_file(ctx);
-    if (!rc) ssl_error(NULL, 0);
+    if (!rc) ssl_error();
 
     tls_state->ssl = SSL_new(tls_state->ctx);
-    if (!tls_state->ssl) ssl_error(NULL, 0);
+    if (!tls_state->ssl) ssl_error();
     if (!getenv(NO_VERIFY)) SSL_set_verify(tls_state->ssl, SSL_VERIFY_PEER, NULL);
 
     shared_tls_init(me, other, tls_state);
