3a1b794ec1a3e34f2edfd17c4392c8bda9bf7e6e
Rainer Weikusat <rweikusat@talktalk.net>
2026-05-30 18:18:24 +0100

    task queue implementation

--- src/tasks.c
+++ src/tasks.c
@@ -9,5 +9,47 @@
 struct task {
     struct task *p;
     task_fn *fn;
-    void *p;
+    void *fn_p;
 };
+
+/*  variables */
+static struct task tsks[2];
+static unsigned nxt_tsk;
+
+static struct {
+    struct task *first, **chain;
+} q = {
+    .chain = &q.first
+};
+
+/*  routines */
+void queue_task(task_fn *fn, void *p)
+{
+    struct task *tsk;
+
+    tsk = tsks + nxt_tst;
+    nxt_tsk ^= 1;
+
+    tsk->fn = fn;
+    tsk->fn_p = p;
+    tsk->p = NULL;
+
+    *q.chain = tsk;
+    q.chain = &tsk->p;
+}
+
+void run_tasks(void)
+{
+    struct task *tsk;
+
+    tsk = q.first;
+    if (!tsk) return;
+
+    q.first = NULL;
+    q.chain = &q.first;
+
+    do {
+        tsk->fn(tsk->fn_p);
+        tsk = tsk->p;
+    } while (tsk);
+}
